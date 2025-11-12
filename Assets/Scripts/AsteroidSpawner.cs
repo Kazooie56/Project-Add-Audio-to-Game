f1e6c14158dc8c656d96b0efcd0822b5055c5e2c
Kazooie56 <w0465155@campus.nscc.ca>
2025-11-11 23:13:45 -0400

    Re-submission

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public int waveWait;
+    public int waveCount = 0;
+
+    public SFXManager sfxManager;
+    private AudioSource backgroundMusic;
+    public float tempoIncreasePercent = 0.05f;
+
+    public bool spawn = true;
+
+
+    void Awake()
+    {
+        if (sfxManager == null)
+        {
+            if (sfxManager == null)
+                sfxManager = FindObjectOfType<SFXManager>();
+
+            if (sfxManager != null)
+                backgroundMusic = sfxManager.GetBgMusicAudioSource();
+        }
+    }
+
+    void Start()
+    {
+        if (sfxManager != null)
+        {
+            backgroundMusic = sfxManager.GetBgMusicAudioSource();
+        }
+        StartCoroutine( SpawnWaves() );        
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+           
+        while (spawn == true) 
+        {
+            for (int w = 0; w < 100; w++)
+            {
+                waveCount = w;
+                hazardCount = hazardCount + 2;
+                float hazardSpeed = -5 + (-1f * waveCount);
+
+                if (backgroundMusic != null)
+                    backgroundMusic.pitch += tempoIncreasePercent; //MAKE SURE YOU DON'T HAVE EXTRA CODE, USE THE NUMBERS ON THE LEFT THAT TELL YOU WHAT LINE YOU'RE ON AS REFERENCE!
+
+                for (int i = 0; i < hazardCount; i++)
+                {
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity; // no rotation, rotation is set on the asteroid script itself.
+                    GameObject asteroidClone = Instantiate(hazard, spawnPosition, spawnRotation);
+
+                    asteroidClone.transform.SetParent(this.transform); //organises all asteroids under AsteroidSpawner in hierachy.
+
+                    Mover asteroidStats = asteroidClone.GetComponent<Mover>();
+                    asteroidStats.speed = hazardSpeed;                    
+
+                    yield return new WaitForSeconds(spawnWait);
+                }
+                yield return new WaitForSeconds(waveWait);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioClip playerShoot;
+    public AudioClip asteroidExplosion;
+    public AudioClip playerDamage;
+    public AudioClip playerExplosion;
+    public AudioClip BgMusicGameplay;
+    public AudioClip BgMusicTitleScreen;
+
+    private AudioSource SFXaudioSource;
+
+    private AudioSource BgMusicAudioSource;
+
+    [SerializeField] float pitchVariance = 0.05f; // this is for the sfx variance
+
+    public void Awake()
+    {
+        SFXaudioSource = GetComponent<AudioSource>();
+
+        //GameObject child = this.transform.Find("BgMusic").gameObject;
+
+        BgMusicAudioSource = gameObject.transform.Find("BgMusic").gameObject.GetComponent<AudioSource>();
+
+
+
+        //BgMusicAudioSource.GetComponent<AudioSource>().Play();       
+    }
+
+
+
+    //called in the PlayerController Script
+    public void PlayerShoot()
+    {
+        float randomPitch = Random.Range(1f - pitchVariance, 1f + pitchVariance);
+        SFXaudioSource.pitch = randomPitch;
+        SFXaudioSource.PlayOneShot(playerShoot);
+    }
+
+    //called in the PlayerController Script
+    public void PlayerDamage()
+    {
+        float randomPitch = Random.Range(1f - pitchVariance, 1f + pitchVariance);
+        SFXaudioSource.pitch = randomPitch;
+        SFXaudioSource.PlayOneShot(playerDamage);
+    }
+
+    //called in the PlayerController Script
+    public void PlayerExplosion()
+    {
+        SFXaudioSource.PlayOneShot(playerExplosion);
+    }
+
+    //called in the AsteroidDestroy script
+    public void AsteroidExplosion()
+    {
+        float randomPitch = Random.Range(1f - pitchVariance, 1f + pitchVariance);
+        SFXaudioSource.pitch = randomPitch;
+        SFXaudioSource.PlayOneShot(asteroidExplosion);
+    }
+
+
+    public void BGMusicMainMenu()
+    {
+        BgMusicAudioSource.clip = BgMusicTitleScreen;
+        BgMusicAudioSource.Play();
+    }
+
+    public void BGMusicGameplay()
+    {
+        BgMusicAudioSource.GetComponent<AudioSource>().clip = BgMusicGameplay;
+        BgMusicAudioSource.Play();
+
+    }
+
+    public AudioSource GetBgMusicAudioSource()
+    {
+        return BgMusicAudioSource;
+    }
+}
